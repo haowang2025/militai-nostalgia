7bc6693cf0186ac7823e9d8e8354f61fbc7cc2a0
Hao Wang <hwang669@asu.edu>
2026-06-20 13:11:10 +0800

    Import long-press Remember range styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import './moment-delete.css';
+import './remember-range.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
