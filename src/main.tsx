a35e599d7f71b214433d878e49d0b2abe3482fea
Hao Wang <hwang669@asu.edu>
2026-06-20 13:33:27 +0800

    Enable robust thumbnail lightbox

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './media-lightbox-dom';
 import './index.css';
 import './moment-delete.css';
 import './remember-range.css';
+import './media-lightbox-dom.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
