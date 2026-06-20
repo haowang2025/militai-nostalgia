31399bb2d34872d9128a242d5231481278e4935b
Hao Wang <hwang669@asu.edu>
2026-06-20 19:24:05 +0800

    Enable Moment anchor hover linking

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,12 @@ import './media-lightbox-dom';
 import './space-remember-dom';
 import './blank-moment-dom';
 import './progress-drag-dom';
+import './moment-anchor-hover-dom';
 import './index.css';
 import './moment-delete.css';
 import './remember-range.css';
 import './media-lightbox-dom.css';
+import './moment-anchor-hover.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
