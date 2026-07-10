552dd874383b24c4987a361c04b6b036c836fd5f
Hao Wang <hwang669@asu.edu>
2026-07-10 15:17:34 +0800

    Remove DOM adapters from app entrypoint

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './media-lightbox-dom';
-import './space-remember-dom';
-import './blank-moment-dom';
-import './progress-drag-dom';
-import './moment-anchor-hover-dom';
 import './index.css';
 import './moment-delete.css';
 import './remember-range.css';
 import './media-lightbox-dom.css';
 import './moment-anchor-hover.css';
+import './quality-improvements.css';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const root = document.getElementById('root');
+if (!root) throw new Error('Missing #root element.');
+
+ReactDOM.createRoot(root).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
