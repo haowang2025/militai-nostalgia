9ffa853ce8caab8ea85fbdfc05ee0b1728beb335
Hao Wang <hwang669@asu.edu>
2026-07-12 01:22:59 +0800

    Route cloud mode and add player entry

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import CloudApp from './CloudApp';
 import './media-lightbox-dom';
 import './space-remember-dom';
 import './blank-moment-dom';
@@ -11,9 +12,13 @@ import './moment-delete.css';
 import './remember-range.css';
 import './media-lightbox-dom.css';
 import './moment-anchor-hover.css';
+import './cloud.css';
+
+const normalizedPath = window.location.pathname.replace(/\/+$/, '');
+const cloudMode = normalizedPath.endsWith('/cloud');
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    {cloudMode ? <CloudApp /> : <><App /><a className="cloud-entry-link" href="/nostalgia/cloud">网易云音乐</a></>}
   </React.StrictMode>,
 );
