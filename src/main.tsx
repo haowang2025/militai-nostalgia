179d0f1cc1f09f4aa0018f519a6d22edcb759f6f
Hao Wang <hwang669@asu.edu>
2026-07-12 01:25:12 +0800

    Isolate local DOM shortcuts from cloud mode

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import CloudApp from './CloudApp';
-import './media-lightbox-dom';
-import './space-remember-dom';
-import './blank-moment-dom';
-import './progress-drag-dom';
-import './moment-anchor-hover-dom';
 import './index.css';
 import './moment-delete.css';
 import './remember-range.css';
@@ -16,9 +11,23 @@ import './cloud.css';
 
 const normalizedPath = window.location.pathname.replace(/\/+$/, '');
 const cloudMode = normalizedPath.endsWith('/cloud');
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    {cloudMode ? <CloudApp /> : <><App /><a className="cloud-entry-link" href="/nostalgia/cloud">网易云音乐</a></>}
-  </React.StrictMode>,
-);
+if (cloudMode) {
+  root.render(<React.StrictMode><CloudApp /></React.StrictMode>);
+} else {
+  const renderLocalApp = () => root.render(
+    <React.StrictMode>
+      <App />
+      <a className="cloud-entry-link" href="/nostalgia/cloud">网易云音乐</a>
+    </React.StrictMode>,
+  );
+
+  void Promise.all([
+    import('./media-lightbox-dom'),
+    import('./space-remember-dom'),
+    import('./blank-moment-dom'),
+    import('./progress-drag-dom'),
+    import('./moment-anchor-hover-dom'),
+  ]).then(renderLocalApp, renderLocalApp);
+}
